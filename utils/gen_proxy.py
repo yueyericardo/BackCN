6ae1cf774bfb9f75235fd113fd9585edb81b98ed
richard <yueyericardo@gmail.com>
2020-03-13 05:12:40 +0000

    .dat util

---
+++ utils/gen_proxy.py
@@ -0,0 +1,14 @@
+proxy = ""
+f = open("backcn", "r")
+for line in f:
+    if line[:2] == "||":
+        proxy += line[2:-1] + '\n'
+
+# print(proxy)
+
+f.close()
+
+# write file
+f = open("site/proxy", "w")
+f.write(proxy)
+f.close()
